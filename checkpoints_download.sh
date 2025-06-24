f4477bb3a6ee71273d4c0c9253a43094da4c746b
FelixYuan-YF <FelixYuan2003@outlook.com>
2025-06-24 22:04:46 +0800

    add test video

--- checkpoints_download.sh
+++ checkpoints_download.sh
@@ -5,9 +5,5 @@ export HF_ENDPOINT=https://hf-mirror.com
 # depth anything
 huggingface-cli download --resume-download depth-anything/Depth-Anything-V2-Large --local-dir Depth-Anything
 
-# raft 
-mkdir -p ./raft/
-gdown -c https://drive.google.com/uc?id=1MqDajR89k-xLV0HIrmJ0k-n8ZpG6_suM -O raft/raft-things.pth
-
 # unidepth
 huggingface-cli download --resume-download lpiccinelli/unidepth-v2-vitl14 --local-dir UniDepth
