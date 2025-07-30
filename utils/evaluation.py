20734988a28f33557565b368b115a98ff928106f
P <130880647+FelixYuan-YF@users.noreply.github.com>
2025-07-30 10:34:22 +0800

    Update evaluation.py

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -11,8 +11,25 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from scipy.ndimage import gaussian_filter
+from scipy.sparse import csr_matrix
 
-from .expand import expand
+
+def expand(loaded_data):
+    reconstructed_sparse_matrices = []
+    num_frames = (len(loaded_data) - 1) // 3
+    matrix_shape = loaded_data['shape']
+
+    for i in range(num_frames):
+        data = loaded_data[f'f_{i}_data']
+        indices = loaded_data[f'f_{i}_indices']
+        indptr = loaded_data[f'f_{i}_indptr']
+        # reconstruction csr_matrix
+        reconstructed_matrix = csr_matrix((data, indices, indptr), shape=matrix_shape)
+        reconstructed_sparse_matrices.append(reconstructed_matrix)
+
+    reconstructed_mask_3d = np.stack([m.toarray() for m in reconstructed_sparse_matrices], axis=0)
+    # reconstructed_mask_3d.shape
+    return reconstructed_mask_3d
 
 
 def load_file(cam_pos_file, mask_file, device):
