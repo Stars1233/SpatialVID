b0204e7590c21b69a72eae2e9271b04c30388552
Felix Yuan <felixyuan2003@outlook.com>
2025-11-05 22:12:13 +0800

    Refactor download paths in download_checkpoints.sh

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -2,14 +2,13 @@ mkdir -p ./checkpoints/
 cd ./checkpoints/
 
 # aesthetic
-wget https://github.com/christophschuhmann/improved-aesthetic-predictor/raw/main/sac+logos+ava1-l14-linearMSE.pth -O checkpoints/aesthetic.pth
+wget https://github.com/christophschuhmann/improved-aesthetic-predictor/raw/main/sac+logos+ava1-l14-linearMSE.pth -O aesthetic.pth
 
 # megasam
-wget https://github.com/mega-sam/mega-sam/blob/main/checkpoints/megasam_final.pth -O checkpoints/megasam_final.pth
+wget https://github.com/mega-sam/mega-sam/blob/main/checkpoints/megasam_final.pth -O megasam_final.pth
 
 # raft
-mkdir -p ./raft/
-gdown -c https://drive.google.com/uc?id=1MqDajR89k-xLV0HIrmJ0k-n8ZpG6_suM -O raft/raft-things.pth
+gdown -c https://drive.google.com/uc?id=1MqDajR89k-xLV0HIrmJ0k-n8ZpG6_suM -O raft-things.pth
 
 # depth anything
 huggingface-cli download --resume-download depth-anything/Depth-Anything-V2-Large --local-dir Depth-Anything
